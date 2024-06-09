37926d0bcb0f6718c419f50ed52093aaf22e9b7d
uddhav.baniya <uddhav.baniya@midastechnologies.com.np>
2024-06-09 13:55:37 +0545

    Bug Fixing

--- packages/Bidhan/Bhadhan/src/resources/views/sql-editor.blade.php
+++ packages/Bidhan/Bhadhan/src/resources/views/sql-editor.blade.php
@@ -4,80 +4,13 @@
 
 @section('content')
     <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism-tomorrow.min.css" rel="stylesheet" />
-    <style>
-        .editor {
-            background-color: #2d2d2d;
-            color: #f8f8f2;
-            padding: 10px;
-            border-radius: 5px;
-            font-family: monospace;
-            min-height: 100px;
-            max-height: 500px;
-            overflow-y: auto;
-            white-space: pre-wrap;
-            outline: none;
-        }
-    
-        .sql-table-div {
-            max-height: 400px;
-            overflow-y: auto;
-        }
-    
-        .sql-table-div table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-    
-        .sql-table-div thead th {
-            position: sticky;
-            top: 0;
-            background-color: #343a40;
-            color: white;
-            z-index: 1;
-            padding: 8px;
-            text-align: center;
-            border: 1px solid #ddd;
-        }
-    
-        .sql-table-div tbody tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-    
-        .sql-table-div tbody tr:nth-child(odd) {
-            background-color: #fff;
-        }
-    
-        .sql-table-div tbody td {
-            padding: 8px;
-            text-align: left;
-            border: 1px solid #ddd;
-            font-family: monospace;
-            color: #333;
-        }
-    
-        .sql-table-div tbody td i {
-            color: #999;
-        }
-    
-        .text-center {
-            text-align: center;
-        }
-    
-        .text-white {
-            color: white;
-        }
-    
-        .f-08 {
-            font-size: 0.8rem;
-        }
-    </style>
-    
     <div id="vue_app">
         <div class="container-fluid">
             <div class="col-12">
                 <div class="form-group">
                     <p class="sql-label mt-1 mb-0">Enter Your SQL Query :</p>
-                    <div id="editor" class="editor" contenteditable="true" @input="updateRawSql" @keydown="handleKeydown" ref="editor"></div>
+                    <div id="editor" class="editor" contenteditable="true" @input="updateRawSql" @keydown="handleKeydown"
+                        ref="editor"></div>
                 </div>
             </div>
             <div :class="sqlData.length ? 'sql-table-div col-12' : 'col-12'" v-if="sqlData">
@@ -92,11 +25,16 @@
                     <tbody>
                         <tr class="f-08 text-white" v-for="(row, rowKey) in sqlData">
                             <template v-for="(columnValue, columnValueKey) in columnNames">
-                                <td class="f-08" v-html="row[columnValue] ? row[columnValue] : '<i>' + null + '</i>'"></td>
+                                <td class="f-08" v-html="row[columnValue] ? row[columnValue] : '<i>' + null + '</i>'">
+                                </td>
                             </template>
                         </tr>
                     </tbody>
                 </table>
+            </div>
+            <div class="col-12 text-center">
+                <p class="sql-label mt-1 mb-0 text-danger-imp" v-text="errMessage">
+                </p>
             </div>
             <div class="row" v-if="messageSummary">
                 <div class="col-12 mt-0 text-center">
@@ -106,7 +44,6 @@
         </div>
     </div>
 @endsection
-
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/prism.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/components/prism-sql.min.js"></script>
@@ -117,26 +54,60 @@
                 rawSql: "",
                 columnNames: [],
                 sqlData: [],
-                messageSummary: null
+                messageSummary: null,
+                errMessage: null
             },
             methods: {
-                updateRawSql: function() {
-                    const editor = this.$refs.editor;
-                    this.rawSql = editor.innerText;
-                    this.$nextTick(function() {
-                        const highlighted = Prism.highlight(this.rawSql, Prism.languages.sql, 'sql');
-                        editor.innerHTML = highlighted;
-                        this.setCaretToEnd(editor);
-                    });
+                getCaretPosition: function(element) {
+                    let caretOffset = 0;
+                    const doc = element.ownerDocument || element.document;
+                    const win = doc.defaultView || doc.parentWindow;
+                    const sel = win.getSelection();
+                    if (sel.rangeCount > 0) {
+                        const range = sel.getRangeAt(0);
+                        const preCaretRange = range.cloneRange();
+                        preCaretRange.selectNodeContents(element);
+                        preCaretRange.setEnd(range.startContainer, range.startOffset);
+                        caretOffset = preCaretRange.toString().length;
+                    }
+                    return caretOffset;
                 },
-                setCaretToEnd: function(editor) {
+                setCaretPosition: function(element, offset) {
                     const range = document.createRange();
                     const sel = window.getSelection();
-                    range.selectNodeContents(editor);
-                    range.collapse(false);
+                    const nodeStack = [element];
+                    let node, charCount = 0;
+                    let foundStart = false;
+
+                    while (node = nodeStack.pop()) {
+                        if (node.nodeType === 3) {
+                            const nextCharCount = charCount + node.length;
+                            if (!foundStart && offset >= charCount && offset <= nextCharCount) {
+                                range.setStart(node, offset - charCount);
+                                foundStart = true;
+                            }
+                            charCount = nextCharCount;
+                        } else {
+                            let i = node.childNodes.length;
+                            while (i--) {
+                                nodeStack.push(node.childNodes[i]);
+                            }
+                        }
+                    }
+                    range.collapse(true);
                     sel.removeAllRanges();
                     sel.addRange(range);
-                    editor.focus();
+                },
+                updateRawSql: function() {
+                    const editor = this.$refs.editor;
+                    const caretPosition = this.getCaretPosition(editor);
+
+                    this.rawSql = editor.innerText;
+                    this.$nextTick(() => {
+                        const highlighted = Prism.highlight(this.rawSql, Prism.languages.sql, 'sql');
+                        editor.innerHTML = highlighted;
+                        this.setCaretPosition(editor, caretPosition);
+                    });
                 },
                 handleKeydown: function(event) {
                     let vm = this;
@@ -153,9 +124,12 @@
                         vm.columnNames = res.data.columnNames;
                         vm.sqlData = res.data.fetchFromSql;
                         vm.messageSummary = res.data.summary;
-                        console.log(res);
+                        vm.errMessage = null;
                     }).catch(function(err) {
-                        console.log(err);
+                        vm.columnNames = [];
+                        vm.sqlData = [];
+                        vm.messageSummary = null;
+                        vm.errMessage = err.response.data;
                     });
                 }
             },
@@ -167,3 +141,6 @@
         });
     </script>
 @endsection
+
+
+
